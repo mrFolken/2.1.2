f21a4b96c59dd01bf1d4bef6a2426dd04f434912
Андрей Фолькенштейн <Folk1992@yandex.ru>
2023-12-08 19:51:13 +0700

    Patch 1.1

--- main.cpp
+++ main.cpp
@@ -1,64 +1,62 @@
-#include <iostream>
-#include <fstream>
-#include <Windows.h>
-#include <ctime>
-#include <vector>
-#include <iomanip>
-#include "Planet.h"
+#include "main.h"
 
 using namespace std;
 
 void main()
 {
+	setupRusLang();
+	vector<Planet> sunSystem;
+	createSunSystemFromFile(enterFileName(), sunSystem);
+	printPlanetSystem(sunSystem);
+}
+
+string enterFileName()
+{
+	string fileName = "Планеты";//Имя файла
+	//cout << "Введите имя файла для загрузки - ";
+	//cin >> fileName;
+	fileName += ".txt";//добавляем формат файла
+	return fileName;
+}
+
+void setupRusLang()
+{
 	setlocale(LC_ALL, "RUS");//русификация
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);//русификация для ввода-вывода имени файла
-	
-	vector<Planet> sunSystem;
-	Planet bufPlanet;
+}
 
-	string fileName = "Планеты";//Имя файла
-	
-	//cout << "Введите имя файла для загрузки - ";
-	//cin >> fileName;
-
-	fileName += ".txt";//добавляем формат файла
+void createSunSystemFromFile(string fileName, vector<Planet> &sunSystem)
+{
 	ifstream fin(fileName);//создаём объект класса ifstream для записи и связываем его с файлом
-
-	tm bufOpenDate;
+	Planet bufPlanet;
 	string bufStr;
-
-	if (fin.is_open())
+	while (!fin.eof())
 	{
-		cout << "Файл успешно открыт!" << endl;
-		while (!fin.eof())
-		{
-			fin >> bufStr;
-			bufPlanet.setNamePlanet(bufStr);
-
-			fin >> bufStr;
-			bufOpenDate.tm_year = stoi(bufStr.substr(0, 4));
-			bufOpenDate.tm_mon = stoi(bufStr.substr(5, 7));
-			bufOpenDate.tm_mday = stoi(bufStr.substr(8, 9));
-			bufPlanet.setOpenDate(bufOpenDate);
-
-			fin >> bufStr;
-			bufPlanet.setRadius(stod(bufStr));
-
-			sunSystem.push_back(bufPlanet);
-		}
+		fin >> bufStr; bufPlanet.setNamePlanet(bufStr);
+		fin >> bufStr; bufPlanet.setOpenDate(stringToTm(bufStr));
+		fin >> bufStr; bufPlanet.setRadius(stod(bufStr));
+		sunSystem.push_back(bufPlanet);
 	}
-	else
-		cout << "Ошибка открытия файла!" << endl;
+	fin.close();//закрываем файл
+}
 
+tm stringToTm(string buf)
+{
+	tm bufOpenDate;
+	bufOpenDate.tm_year = stoi(buf.substr(0, 4));
+	bufOpenDate.tm_mon = stoi(buf.substr(5, 7));
+	bufOpenDate.tm_mday = stoi(buf.substr(8, 9));
+	return bufOpenDate;
+}
 
-	cout << "Название планеты | Дата открытия | Радиус"<< endl;
+void printPlanetSystem(vector<Planet> &sunSystem)
+{
+	cout << "Название планеты | Дата открытия | Радиус" << endl;
 	for (int i = 0; i < sunSystem.size(); i++)
 	{
 		cout << setw(18) << left << sunSystem[i].getNamePlanet() << " "
 			<< setw(4) << right << sunSystem[i].getOpenDate().tm_year << "." << setw(2) << sunSystem[i].getOpenDate().tm_mon << "." << setw(2) << sunSystem[i].getOpenDate().tm_mday << setw(6) << " "
 			<< setw(10) << left << sunSystem[i].getRadius() << endl;
 	}
-
-	fin.close();//закрываем файл
 }
